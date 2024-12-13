332dfcc8cc68e6f6445e94b5246810f4886bc597
Joseph Jebara <122958432+jjebara26@users.noreply.github.com>
2024-12-13 12:57:35 -0700

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -7,9 +7,7 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score
 
 torch.manual_seed(42)
 
-# recommended preprocessing steps: resize to square -> convert to tensor -> normalize the image
-# if you are resizing, 100 is a good choice otherwise GradeScope will time out
-# you could use Compose (https://pytorch.org/vision/stable/generated/torchvision.transforms.Compose.html) from transforms module to handle preprocessing more conveniently
+# preprocessing
 transform = transforms.Compose([
     transforms.Resize((100, 100)),
     transforms.ToTensor(),
@@ -17,34 +15,28 @@ transform = transforms.Compose([
 ])
 
 
-# thanks to torchvision, this is a convenient way to read images from folders directly without writing datasets class yourself (you should know what datasets class is as mentioned in the documentation)
+# read images from folder
 dataset = datasets.ImageFolder(root='./petimages', transform=transform)
 
 
-# now we need to split the data into training set and evaluation set 
-# use 20% of the dataset as test
+# splitting the data into training set and evaluation set 
+# 20% of the dataset as test
 generator = torch.Generator().manual_seed(42)
 test_set, train_set = torch.utils.data.random_split(dataset, [int(0.2 * len(dataset)), int(0.8 * len(dataset))], generator=generator)
-# prepare dataloader for training set and evaluation set
+
+# preparing dataloader for training set and evaluation set
 trainloader = torch.utils.data.DataLoader(train_set, batch_size=32, shuffle=True)
 testloader = torch.utils.data.DataLoader(test_set, batch_size=32, shuffle=False)
 
-# model hyperparameter
+# model hyperparameters
 learning_rate = 0.0001
 batch_size = 32
 epoch_size = 10
 
 
 
-# model design goes here
+# model design
 class CNN(nn.Module):
-
-    # there is no "correct" CNN model architecture for this lab, you can start with a naive model as follows:
-    # convolution -> relu -> pool -> convolution -> relu -> pool -> convolution -> relu -> pool -> linear -> relu -> linear -> relu -> linear
-    # you can try increasing number of convolution layers or try totally different model design
-    # convolution: nn.Conv2d (https://pytorch.org/docs/stable/generated/torch.nn.Conv2d.html)
-    # pool: nn.MaxPool2d (https://pytorch.org/docs/stable/generated/torch.nn.MaxPool2d.html)
-    # linear: nn.Linear (https://pytorch.org/docs/stable/generated/torch.nn.Linear.html)
 
     def __init__(self):
         super(CNN, self).__init__()
@@ -69,17 +61,19 @@ class CNN(nn.Module):
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu' # whether your device has GPU
 cnn = CNN().to(device) # move the model to GPU
+
 # search in official website for CrossEntropyLoss
 criterion = nn.CrossEntropyLoss()
-# try Adam optimizer (https://pytorch.org/docs/stable/generated/torch.optim.Adam.html) with learning rate 0.0001, feel free to use other optimizer
+
+# Adam optimizer with learning rate 0.0001
 optimizer = optim.Adam(cnn.parameters(), lr=learning_rate)
 
 
 # start model training
-cnn.train() # turn on train mode, this is a good practice to do
-for epoch in range(epoch_size): # begin with trying 10 epochs 
+cnn.train() # turning on train mode
+for epoch in range(epoch_size): # trying 10 epochs 
 
-    loss = 0.0 # you can print out average loss per batch every certain batches
+    loss = 0.0 # average loss
 
     for i, data in enumerate(trainloader, 0):
         # get the inputs and label from dataloader
@@ -88,9 +82,9 @@ for epoch in range(epoch_size): # begin with trying 10 epochs
         inputs = inputs.to(device)
         labels = labels.to(device)
 
-        # zero the parameter gradients using zero_grad()
+        # zeroing the parameter gradients
         optimizer.zero_grad()
-        # forward -> compute loss -> backward propogation -> optimize (see tutorial mentioned in main documentation)
+        # forward -> compute loss -> backward propogation -> optimize
         outputs = cnn(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
@@ -108,20 +102,18 @@ print('Finished Training')
 # evaluation on evaluation set
 ground_truth = []
 prediction = []
-cnn.eval() # turn on evaluation model, also a good practice to do
-with torch.no_grad(): # since we're not training, we don't need to calculate the gradients for our outputs, so turn on no_grad mode
+cnn.eval() # turning on evaluation model
+with torch.no_grad(): # not training, so we don't need to calculate the gradients for our outputs
     for data in testloader:
         inputs, labels = data
         inputs = inputs.to(device)
         ground_truth += labels.tolist() # convert labels to list and append to ground_truth
-        # calculate outputs by running inputs through the network
+        # calculating outputs by running inputs through the network
         outputs = cnn(inputs)
         # the class with the highest logit is what we choose as prediction
         _, predicted = torch.max(outputs, 1)
         prediction += predicted.tolist() # convert predicted to list and append to prediction
 
-
-# GradeScope is chekcing for these three variables, you can use sklearn to calculate the scores
 accuracy = accuracy_score(ground_truth, prediction)
 recall = recall_score(ground_truth, prediction)
 precision = precision_score(ground_truth, prediction)
